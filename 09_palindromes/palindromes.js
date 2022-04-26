32c06df6030931ecfa303bad6a4233150c4b2cf1
Simon Y <therealkjai139@gmail.com>
2022-04-26 19:54:49 -0400

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    newStr = str.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+    newStr = newStr.toLowerCase()
+    console.log(newStr)
+    splitStr = newStr.split('')
+    reverseStr = splitStr.reverse().join('')
+    console.log(reverseStr)
+    if (newStr == reverseStr){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
